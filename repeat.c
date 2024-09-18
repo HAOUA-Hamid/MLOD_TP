a966d68bda439f3d29950a92a332a00a6477563c
hamid haoua <hamidhaoua15@gmail.com>
2024-09-18 15:22:03 +0200

    version 2 taille dynamic

--- repeat.c
+++ repeat.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *repeat(int *t1, int *t2,int *total_size) {
+int *repeat(int *t1, int *t2, int size, int *total_size) {
     *total_size = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < size; i++) {
         *total_size += t1[i];
     }
     int *t = (int *)malloc(*total_size * sizeof(int));
     int k = 0;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < size; i++) {
         for (int j = 0; j < t1[i]; j++) {
             t[k] = t2[i];
             k++;
@@ -19,10 +19,11 @@ int *repeat(int *t1, int *t2,int *total_size) {
 }
 
 int main() {
+    int size=3;
     int t1[]={1,2,4};
     int t2[]={10,3,8};
     int total_size;
-    int *t = repeat(t1, t2,&total_size);
+    int *t = repeat(t1, t2, size, &total_size);
     for (int i = 0; i < total_size; i++) {
         printf("%d ", t[i]);
     }
