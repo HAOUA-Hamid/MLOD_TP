f2880a468de9b39c06b9e59d075dcfc28994fd4c
hamid haoua <hamidhaoua15@gmail.com>
2024-09-18 15:46:00 +0200

    version semi final better UI

--- repeat.c
+++ repeat.c
@@ -19,14 +19,45 @@ int *repeat(int *t1, int *t2, int size, int *total_size) {
 }
 
 int main() {
-    int size=3;
-    int t1[]={1,2,4};
-    int t2[]={10,3,8};
+    int size;
+    printf("Enter the size of the arrays: ");
+    scanf("%d", &size);
+    int *t1 = (int *)malloc(size * sizeof(int));
+    int *t2 = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        if((i+1)%10==1){
+            printf("Enter the %d st elements of t1: ",i+1);
+        }else if((i+1)%10==2){
+            printf("Enter the %d nd elements of t1: ",i+1);
+        }else if((i+1)%10==3){
+            printf("Enter the %d rd elements of t1: ",i+1);
+        }else{
+             printf("Enter the %d th elements of t1: ",i+1);
+        }
+        scanf("%d", &t1[i]);
+    }
+    for (int i = 0; i < size; i++) {
+        if(((i+1)!=11) && ((i+1)%10==1)){
+            printf("Enter the %d st elements of t2: ",i+1);
+        }else if(((i+1)!=12) && (i+1)%10==2){
+            printf("Enter the %d nd elements of t2: ",i+1);
+        }else if(((i+1)!=13) && (i+1)%10==3){
+            printf("Enter the %d rd elements of t2: ",i+1);
+        }else{
+             printf("Enter the %d th elements of t2: ",i+1);
+        }
+        scanf("%d", &t2[i]);
+    }
     int total_size;
     int *t = repeat(t1, t2, size, &total_size);
+    printf("Result array: ");
     for (int i = 0; i < total_size; i++) {
         printf("%d ", t[i]);
     }
+    printf("\n");
     free(t);
+    free(t1);
+    free(t2);
+
     return 0;
 }
